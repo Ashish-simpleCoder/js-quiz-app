79dda3616b6a96ba05af5faeda09fe21f3a6dfc5
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-11-21 12:50:05 +0530

    Updates

---
+++ script/questionRenderer.js
@@ -0,0 +1,34 @@
+export default function questionRenderer( question_arr, marks ){
+   const arr_question = question_arr[window.q_index]
+   window.clicked = false
+
+   options.innerHTML = ''
+   question.innerHTML = arr_question.q
+
+   q_number.textContent = 'Q. '+parseInt(window.q_index+1)
+   arr_question.options.forEach(option=> {
+      const li = document.createElement('li')
+      li.id = option
+      if(option.length > 50) li.style.maxWidth = '100%'
+      li.textContent = option
+
+      li.addEventListener('click',()=>{
+         if(window.clicked) return
+         window.clicked = true
+         li.id !== arr_question.true_ans && (li.style.background = 'red')
+         if(li.id === arr_question.true_ans){
+            li.style.background = 'green'
+            marks++
+         }
+         setTimeout(async()=>{
+            window.q_index<question_arr.length-1 && (
+               window.q_index++,
+               // (await import('./question_renderer.js')).default(question_arr,marks)
+               questionRenderer(question_arr,marks)
+            )
+         },1000)
+      })
+
+      options.appendChild(li)
+   } )
+}
