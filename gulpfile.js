50ec03045d8e877693e6fbb4756e386e93fe4d57
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-08 13:07:12 +0530

    theme change making body bg dimmer making question container lighter than body changing bg of home_page_link to more darker

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
-function styleBuilder(){
-    return src('./dist/styles/scss/quiz/quiz.page.scss').pipe(sass()).pipe(dest('dist/styles/css/quiz'))
-}
-function watcher(){
-    watch(['./dist/styles/scss/**/*.scss'],styleBuilder)
-}
 // function styleBuilder(){
-//     return src('./dist/styles/scss/home/home.page.scss').pipe(sass()).pipe(dest('dist/styles/css/home'))
+//     return src('./dist/styles/scss/quiz/quiz.page.scss').pipe(sass()).pipe(dest('dist/styles/css/quiz'))
 // }
 // function watcher(){
 //     watch(['./dist/styles/scss/**/*.scss'],styleBuilder)
 // }
+function styleBuilder(){
+    return src('./dist/styles/scss/home/home.page.scss').pipe(sass()).pipe(dest('dist/styles/css/home'))
+}
+function watcher(){
+    watch(['./dist/styles/scss/**/*.scss'],styleBuilder)
+}
 exports.default = series(styleBuilder,watcher)
