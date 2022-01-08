507257d79ab1a19be4f59e9424dfc8c4b9c1e603
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-08 12:47:24 +0530

    v2 completed added new page for rendering the questions added index.js as main file added fix for dark mode retreivel from local storage

--- dist/script/handleQuestionUpdation.js
+++ dist/script/handleQuestionUpdation.js
@@ -1,50 +1,25 @@
 await import('./darkMode.js')
 home_page_link.addEventListener('click',()=>history.back())
 
-
 let questions_arr = []
-
-// const l = location.toString()
-// if(l.includes('medium')){
-//    questions_arr = (await import('../public/api/medium_level_questions.js')).default
-//    console.log('medium')
-//    console.log(questions_arr)
-// }else if(l.includes('basic')){
-//    questions_arr = (await import('../public/api/basic_level_questions.js')).default
-//    console.log('basic')
-//    console.log(questions_arr)
-// }else{
-//    questions_arr = (await import('../public/api/hard_level_questions.js')).default
-//    console.log('hard')
-//    console.log(questions_arr)
-// }
-
-// window.q_index = questions_arr.length-1
-// window.q_index = 0
-// window.clicked = false
-// window.marks = 0
-
-
 
 window.q_index = 0
 window.clicked = false
 window.marks = 0
 
+// getting the level name from query string
+const level_name = location.search.slice(location.search.indexOf('=')+1)
 
-const l = location.toString()
-if(l.includes('medium')){
-   import('../public/api/medium_level_questions.js').then(data=>{
-      import('./questionRenderer.js').then(f=>f.default(data.default,marks))
+// rendering the page dynamically from query string
+pageRenderer(import(`../public/api/${level_name}_level_questions.js`))
+function pageRenderer(instruction){
+   instruction.then(({default:question_arr})=>{
+      import('./questionRenderer.js').then(({default:func})=>{
+         func(question_arr,marks, q_index)
+      })
    })
 }
-else if(l.includes('basic')){
-   import('../public/api/basic_level_questions.js').then(data=>{
-      import('./questionRenderer.js').then(f=>f.default(data.default,marks))
-   })
-}
-else{
-   import('../public/api/hard_level_questions.js').then(data=>{
-      import('./questionRenderer.js').then(f=>f.default(data.default,marks))
-   })
-}
+
+// insrting the level name in the span tag with id=level
+level.textContent = level_name
 
--- dist/script/questionRenderer.js
+++ dist/script/questionRenderer.js
@@ -1,43 +1,58 @@
-export default async function questionRenderer( question_arr, marks ){
-   // loader.style.display = 'flex'
-   const arr_question = question_arr[window.q_index]
-   // const progress_bar_percent = (window.q_index/question_arr.length)*100
-   // console.log(progress_bar_percent)
+export default async function questionRenderer(question_arr, marks, q_index ){
    window.clicked = false
-
+   const current_question = question_arr[q_index]
    options.innerHTML = ''
-   question.innerHTML = arr_question.q
+   question.innerHTML = current_question.q
 
-   imgUpdater(arr_question.img)    //updating the img
+   imgUpdater(current_question.img)    //updating the img
+   q_number.textContent = 'Q. '+parseInt(q_index+1)      //updating the question number like 1,2,3, and so on
 
-   q_number.textContent = 'Q. '+parseInt(window.q_index+1)      //updating the question
+   current_question.options.forEach(option=> {
+      const obj = {option,question_arr, q_index, current_question, marks}
+      renderOptions(obj)
+   })
+}
 
-   arr_question.options.forEach(option=> {
-      const li = document.createElement('li')
-      li.id = option
-      if(option.length > 50) li.style.maxWidth = '100%'
-      li.textContent = option
 
-      li.addEventListener('click',()=>{
-         updateProgressBar(question_arr)
-         if(window.clicked) return
-         window.clicked = true
-         li.id !== arr_question.true_ans && (li.style.background = 'red')
-         if(li.id === arr_question.true_ans){
-            li.style.background = 'green'
-            marks++
-         }
-         setTimeout(async()=>{
-            window.q_index<question_arr.length-1 && (
-               window.q_index++,
-               questionRenderer(question_arr,marks)
-               )
-         },1000)
-      })
 
-      options.appendChild(li)
-      loader.style.display = 'none'
-   } )
+function renderOptions(obj){
+   let {option,question_arr, q_index, current_question, marks} = obj
+   const li = document.createElement('li')
+   li.id = option
+   if(option.length > 50) li.style.maxWidth = '100%'
+   li.textContent = option
+
+   li.addEventListener('click',()=>{
+      updateProgressBar(question_arr, q_index)
+      const listenerParams = {li, current_question, marks, q_index, question_arr}
+      optionClickListener(listenerParams)
+   })
+
+   options.appendChild(li)
+   loader.style.display = 'none'
+}
+
+
+function optionClickListener(listenerParams){
+   let {li, current_question, marks, q_index, question_arr} = listenerParams
+   if(window.clicked) return
+   else{
+      window.clicked = true
+      if(li.id === current_question.true_ans){
+         li.style.background = 'green'
+         marks++
+      }else{
+         li.style.background = 'red'
+      }
+   }
+
+   // after selecting any option call the questionRenderer() function again after specified time
+   setTimeout(async()=>{
+      q_index<question_arr.length-1 && (
+         q_index++,
+         questionRenderer(question_arr,marks, q_index)
+         )
+   },1000)
 }
 
 
@@ -45,7 +60,7 @@ function imgUpdater(img){
    img_loader.style.display = 'flex'
    const question_img = document.querySelector('#question_img')
    if(img){
-      question_img.classList.add('question_img')
+      question_img.classList.add('question_img', img)
       question_img.src = img  //updating question img
    }else{
       question_img.src =  '../public/imgs/js_256.png' //updating question img
@@ -55,7 +70,6 @@ function imgUpdater(img){
    img_loader.style.display = 'none'
 }
 
-function updateProgressBar(question_arr){
-   progress_bar.style.width = (((window.q_index+1)/question_arr.length)*100)+'%'   //increasing width
-
+function updateProgressBar(question_arr, q_index){
+   progress_bar.style.width = (((q_index+1)/question_arr.length)*100)+'%'   //increasing width
 }
