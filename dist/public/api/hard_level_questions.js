643b36d80b2627c3db79b1f93204e045ab96b99b
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2022-01-20 16:55:28 +0530

    added 5 questions in hard level with images todo: add more questions in hard level todo: fix margin-top of question displayer in mobile devices

--- dist/public/api/hard_level_questions.js
+++ dist/public/api/hard_level_questions.js
@@ -1,9 +1,33 @@
 const hard_questions_arr = [
    {
+      q:'Which of the following function is technically Higher Order Function in Javascript ?',
+      img:'../public/imgs/hard_imgs/higher_order_func.png',
+      options:["firstClassFunc", 'higherOrder'],
+      true_ans:'firstClassFunc'
+   },
+   {
       q:'What will be the outout of following code ?',
-      img:'../public/imgs/basic_imgs/obj_change.png',
-      options:["js","javascript","JS","Javascript"],
-      true_ans:'Javascript'
+      img:'../public/imgs/hard_imgs/increment_numb.png',
+      options:["15","17","18","16"],
+      true_ans:'18'
+   },
+   {
+      q:'What will be the outout of following code ?',
+      img:'../public/imgs/hard_imgs/greater_condition.png',
+      options:["false", 'true'],
+      true_ans:'false'
+   },
+   {
+      q:'What will be the outout of following code ?',
+      img:'../public/imgs/hard_imgs/boolean_addition.png',
+      options:["1", '5', '6' , '2'],
+      true_ans:'1'
+   },
+   {
+      q:'What will be the outout of following code ?',
+      img:'../public/imgs/hard_imgs/smaller_condition.png',
+      options:["false",'true'],
+      true_ans:'true'
    },
    {
       q:'What is the full form of <b>C.O.V.E</b>',
